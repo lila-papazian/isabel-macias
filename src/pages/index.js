7a349fbc83c091793726406bae13a337c1bb25dc
Lila Papazian <lilapapazian@gmail.com>
2023-07-04 15:20:13 -0300

    add index page + styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,107 @@
-import * as React from "react"
+import * as React from "react";
+import "../css/styles.css";
+import "../i18n";
+import { useTranslation } from "react-i18next";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const lngs = {
+  en: { nativeName: "EN" },
+  es: { nativeName: "ES" },
+};
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const socialLink = {
+  vimeo: "https://vimeo.com/isabelmacias",
+  instagram: "https://www.instagram.com/isabelmacias_arte/",
+  email: "mailto:isabelmacias.art@gmail.com",
+};
 
 const IndexPage = () => {
+  const { t, i18n } = useTranslation();
+  const handleSocialNavigation = (e, key) => {
+    e.preventDefault();
+    window.location = socialLink[key];
+  };
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+    <main>
+      <nav class="navbar navbar-expand-lg">
+        <div className="d-flex w-100 justify-content-end align-items-center me-3">
+          <button
+            className="nav-link"
+            key={"en"}
+            style={{
+              fontWeight: i18n.resolvedLanguage === "en" ? "bold" : "normal",
+              paddingRight: "0.5em",
+            }}
+            type="submit"
+            onClick={() => i18n.changeLanguage("en")}
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+            {lngs["en"].nativeName}
+          </button>
+          |
+          <button
+            className="nav-link"
+            key={"es"}
+            style={{
+              fontWeight: i18n.resolvedLanguage === "es" ? "bold" : "normal",
+              paddingLeft: "0.5em",
+            }}
+            type="submit"
+            onClick={() => i18n.changeLanguage("es")}
+          >
+            {lngs["es"].nativeName}
+          </button>
+        </div>
+      </nav>
+      <div className="d-flex flex-column justify-content-center align-items-center h-100">
+        <h1 className="title mb-3 text-center">Isabel Macias</h1>
+        <span className="subtitle mb-3">{t("comingSoon")}</span>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="icons" onClick={(e) => handleSocialNavigation(e, "vimeo")}>
+            <i
+              className="icon bi bi-vimeo px-3"
+              style={{ color: "#86c9ef" }}
+              
+            ></i>
+
+            <a href={socialLink["vimeo"]} style={{ textDecoration: "none" }}>
+              vimeo.com/isabelmacias
+            </a>
+          </div>
+          <div
+            className="icons"
+            onClick={(e) => handleSocialNavigation(e, "instagram")}
+          >
+            <i
+              className="icon bi bi-instagram px-3"
+              style={{
+                background:
+                  "-webkit-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)",
+                backgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            ></i>
+            <a
+              href={socialLink["instagram"]}
+              style={{ textDecoration: "none" }}
+            >
+              instagram.com/isabelmacias_arte
+            </a>
+          </div>
+          <div className="icons">
+            <i
+              aria-label="Email"
+              className="icon bi bi-envelope px-3"
+              onClick={(e) => handleSocialNavigation(e, "email")}
+            ></i>
+
+            <span>isabelmacias.art@gmail.com</span>
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Isabel Macias</title>;
