df1c8e28525484cca563cd56b690459f2cb23b28
Lila Papazian <lilapapazian@gmail.com>
2024-10-03 13:51:39 -0300

    Feature/rework mampujan page (#18)
    * update mampujan assets
    
    * update translations
    
    * update mampujan layout
    
    * add key for map

--- src/app/[locale]/tejedoras-de-mampujan/page.tsx
+++ src/app/[locale]/tejedoras-de-mampujan/page.tsx
@@ -1,239 +1,101 @@
 "use client";
+import "./../../reset.css";
 import "./../../globals.css";
 import "./globals.css";
-import footer from "./../../../../public/mampujan/pie de página.webp";
 
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+import headerImgEn from "../../../../public/mampujan/headerMampujanEn.webp";
+import headerImgEs from "../../../../public/mampujan/headerMampujanEs.webp";
 
 import { useTranslations } from "next-intl";
 
 import Image from "next/image";
-import Script from "next/script";
-import MampujanCarousel from "@/components/MampujanCarousel";
 
-export default function TejedorasDeMampujan() {
+import FramedContent from "@/components/FramedContent";
+import Carousel from "@/components/Carousel";
+import { carouselOptions, MAMPUJAN_SLIDES, MAMPUJAN_STORYBOARD } from "@/utils";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
+import footer from "../../../../public/mampujan/footer.webp";
+import CustomAccordion from "@/components/CustomAccordion";
+
+export default function TejedorasDeMampujan({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   const t = useTranslations("Mampujan");
 
   return (
-    <main>
-      <AppBar
-        position='static'
-        sx={{ backgroundColor: "transparent", boxShadow: "none" }}
-      >
-        <Toolbar sx={{ justifyContent: "flex-start", alignItems: "center" }}>
-          <a
-            href='/'
-            className='button-menu'
-            style={{
-              color: "black",
-              textDecoration: "none",
-              fontWeight: "bold",
-            }}
-          >
-            <Typography>Isabel Macias</Typography>
-          </a>
-        </Toolbar>
-      </AppBar>
-
-      <div
-        className='header'
-        style={{ width: "100%", display: "flex", justifyContent: "center" }}
-      >
-        <img
-          src='/mampujan/portada web.webp'
-          style={{
-            objectFit: "contain",
-            width: "50%",
-            height: "auto",
-          }}
+    <div style={{ backgroundColor: "#EBE9D4", color: "#37310D" }}>
+      <header>
+        {locale === "es" ? (
+          <Image
+            src={headerImgEs}
+            alt='Isabel Macias presenta Tejedoras de Mampujan'
+            className='header-img'
+          />
+        ) : (
+          <Image
+            src={headerImgEn}
+            alt='Isabel Macias presents Weavers of Mampujan'
+            className='header-img'
+          />
+        )}
+      </header>
+      <main className='main'>
+        <CustomAccordion
+          summary={t("synopsis")}
+          content={t.rich("synopsisDescription", { br: () => <br /> })}
         />
-        {/* <img
-          src="/mampujan/un film de isabel macias v2.webp"
-          style={{
-            objectFit: "contain",
-            width: "50%",
-            height: "auto",
-          }}
-        /> */}
-      </div>
 
-      <div className='body' style={{ marginBottom: "96px" }}>
-        <Box
-          display='flex'
-          justifyContent='center'
-          flexDirection='column'
-          sx={{ paddingBottom: "24px" }}
-        >
-          <Typography
-          variant="h5"
-            sx={{ paddingBottom: "12px", fontWeight: 700 }}
-          >
-            {t("Synopsis")}
-          </Typography>
-          <Typography className='text'>
-            {t.rich("Synopsis Text", { br: () => <br /> })}
-          </Typography>
-        </Box>
+        <FramedContent frameSrc='/mampujan/frame.webp'>
+          <iframe
+            src='https://player.vimeo.com/video/964933926'
+            title='Mampujan - Reel'
+            style={{ border: "none", width: "80%", height: "100%" }}
+          />
+        </FramedContent>
 
-        <div id='vimeo' style={{ paddingBottom: "24px" }}>
-          <Typography
-          variant="h5"
-          sx={{
-              paddingBottom: 0,
-              fontWeight: 700,
-              marginBottom: "-24px",
-            }}
-          >
-            Trailer
-          </Typography>
-          <div
-            style={{
-              width: "100%",
-              color: "transparent",
-              backgroundColor: "transparent",
-            }}
-          >
-            <div
-              style={{
-                padding: "56.25% 0 0 0",
-                position: "relative",
-                margin: "0 auto",
-                width: "80%",
-                height: "80%",
-              }}
-            >
-              <iframe
-                src='https://player.vimeo.com/video/961437058?h=1711145aae&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&muted=1&title=0&byline=0&portrait=0&loop=1&autoplay=1&&controls=false'
-                allow='autoplay; fullscreen;'
-                allowFullScreen
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  width: "100%",
-                  height: "100%",
-                  margin: "auto",  outline: "none", border: "none", boxShadow: "none" 
-                }}
-                title='Tejedoras de Mampujan'
-              ></iframe>
-            </div>
-          </div>
-          <Script id='vimeo-script'>{`https://player.vimeo.com/api/player.js`}</Script>
-        </div>
-
-        {/* <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Typography>Storyboard</Typography>
-        <Masonry columns={3} spacing={2}>
-
-        </Masonry>
-      </Box> */}
-        <Box
-          display='flex'
-          justifyContent='center'
-          flexDirection='column'
-          sx={{ paddingBottom: "24px" }}
-        >
-          <Typography
-          variant="h5"
-          sx={{ paddingBottom: "12px", fontWeight: 700 }}
-          >
-            {t("Behind the scenes")}
-          </Typography>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <MampujanCarousel />
-          </div>
-        </Box>
-
-        <Box
-          display='flex'
-          justifyContent='center'
-          flexDirection='column'
-          sx={{ paddingBottom: "24px" }}
-        >
-          <Typography
-          variant="h5"
-          sx={{ paddingBottom: "12px", fontWeight: 700 }}
-          >
-            {t("Credits")}
-          </Typography>
-          <Typography className='text'>
-            {t.rich("Credits first column", { br: () => <br /> })}
-            {t.rich("Credits second column", { br: () => <br /> })}
-          </Typography>
-        </Box>
-
-        {/* <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Typography>Kit de Prensa</Typography>
-      </Box> */}
-
-        <Box
-          display='flex'
-          gap='6px'
-          sx={{ marginBottom: "96px" }}
-          alignItems='center'
-        >
-          <Typography
-            className='text-large'
-            sx={{ paddingBottom: "12px", fontWeight: 700 }}
-          >
-            {`${t("Contact us")} or ${t("Email").toLocaleLowerCase()} `}{" "}
-            <a
-              href='mailto:info@animation1908.com'
-              className='button-menu'
-              style={{
-                color: "black",
-                textDecoration: "none",
-                fontWeight: 700,
-                fontSize: "larger",
-              }}
-            >
-              info@animation1908.com
-            </a>
-          </Typography>
-        </Box>
-      </div>
-
-      <div
-        className='footer'
-        style={{
-          marginTop: "24px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <Image
-          src={footer}
-          alt='Footer'
-          style={{
-            width: "100%",
-            height: "auto",
-          }}
+        <CustomAccordion
+          summary={t("directorsStatement")}
+          content={t.rich("directorsStatementDescription", {
+            br: () => <br />,
+          })}
         />
+
+        <FramedContent frameSrc='/mampujan/frame.webp'>
+          <Carousel
+            slides={MAMPUJAN_SLIDES}
+            options={carouselOptions}
+            className='carousel-correction'
+          />
+        </FramedContent>
+
+        <h3>Storyboard</h3>
         <div
-          className='text-small'
           style={{
-            paddingBottom: "0.75rem",
-            color: "#E4BE6A",
-            zIndex: 3,
-            position: "absolute",
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "2rem",
           }}
         >
-          {t("MampujanWeavers")}-{t("footer")}
+          {MAMPUJAN_STORYBOARD.map((story: StaticImport, index: number) => (
+            <Image
+              key={index}
+              src={story}
+              alt={`Story ${index}`}
+              style={{ height: "50%", objectFit: "cover" }}
+            />
+          ))}
         </div>
-      </div>
-    </main>
+
+        <p>{t.rich("credits", { br: () => <br /> })}</p>
+      </main>
+      <footer className='footer'>
+        <Image src={footer} alt='footer' className='header-img' />
+      </footer>
+    </div>
   );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -10,22 +10,34 @@ import nina3 from "../public/home/homeCarousel/Nina fotograma03web.jpg";
 import sarna1 from "../public/home/homeCarousel/sarna stop_00111.jpg";
 import sarna2 from "../public/home/homeCarousel/sarna stop_00532web.jpg";
 
-import mampujanMaking1 from "../public/mampujan/carousel/Making 01.jpg";
-import mampujanMaking2 from "../public/mampujan/carousel/Making 02.jpg";
-import mampujanMaking3 from "../public/mampujan/carousel/Making 03.jpg";
-import mampujanMaking4 from "../public/mampujan/carousel/making 04.jpg";
-import mampujanMaking5 from "../public/mampujan/carousel/making 05.jpg";
-import mampujanMaking6 from "../public/mampujan/carousel/making 06.jpg";
-import mampujanMaking7 from "../public/mampujan/carousel/making 07.jpg";
-import mampujanMaking8 from "../public/mampujan/carousel/making 08.jpg";
-import mampujanMaking9 from "../public/mampujan/carousel/making 09.jpg";
-
 import memoryMaking1 from "../public/memory/making/Making00.webp";
 import memoryMaking2 from "../public/memory/making/Making01.webp";
 import memoryMaking3 from "../public/memory/making/Making02.webp";
 import memoryMaking4 from "../public/memory/making/Making03.webp";
 import memoryMaking5 from "../public/memory/making/Making04.webp";
 import memoryMaking6 from "../public/memory/making/Making05.webp";
+
+import making0 from "../public/mampujan/carousel/making 00.webp";
+import making1 from "../public/mampujan/carousel/Making 01.webp";
+import making2 from "../public/mampujan/carousel/Making 02.webp";
+import making3 from "../public/mampujan/carousel/Making 03.webp";
+import making4 from "../public/mampujan/carousel/making 04.webp";
+import making5 from "../public/mampujan/carousel/making 05.webp";
+import making6 from "../public/mampujan/carousel/making 06.webp";
+import making7 from "../public/mampujan/carousel/making 07.webp";
+import making8 from "../public/mampujan/carousel/making 08.webp";
+import making9 from "../public/mampujan/carousel/making 09.webp";
+
+import story1 from "../public/mampujan/storyboard/Story01.webp";
+import story2 from "../public/mampujan/storyboard/Story02.webp";
+import story3 from "../public/mampujan/storyboard/Story03.webp";
+import story4 from "../public/mampujan/storyboard/Story04.webp";
+import story5 from "../public/mampujan/storyboard/Story05.webp";
+import story6 from "../public/mampujan/storyboard/Story06.webp";
+import story7 from "../public/mampujan/storyboard/Story07.webp";
+import story8 from "../public/mampujan/storyboard/Story08.webp";
+
+
 
 export const HOME_SLIDES = [
   banquina1,
@@ -42,15 +54,27 @@ export const HOME_SLIDES = [
 ];
 
 export const MAMPUJAN_SLIDES = [
-  mampujanMaking1,
-  mampujanMaking2,
-  mampujanMaking3,
-  mampujanMaking4,
-  mampujanMaking5,
-  mampujanMaking6,
-  mampujanMaking7,
-  mampujanMaking8,
-  mampujanMaking9,
+  making0,
+  making1,
+  making2,
+  making3,
+  making4,
+  making5,
+  making6,
+  making7,
+  making8,
+  making9,
+];
+
+export const MAMPUJAN_STORYBOARD = [
+  story1,
+  story2,
+  story3,
+  story4,
+  story5,
+  story6,
+  story7,
+  story8,
 ];
 
 export const MEMORY_SLIDES = [
